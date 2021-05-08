e9c8eb1e0d9c26290901382c0f569a57d903ca0a
Mateusz Łaś <mateuszlas3@gmail.com>
2021-05-08 12:47:14 +0200

    Added BaseRespone

---
+++ ModelsDB/Respone/BaseRespone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Models.Respone
+{
+    public class BaseRespone : HttpResponseMessage
+    {
+        public int Succes { get; set; }
+    
+        public string Message { get; set; }
+
+        public int ErrorCode { get; set; }
+
+    }
+}
