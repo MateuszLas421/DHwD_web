40013f40228159e0f3a8a4b98062037d12baec42
MateuszLas421 <mateuszlas3@gmail.com>
2021-06-06 16:49:08 +0200

    Added Read RecommendedStartingPlace

--- DHwD_web/Dtos/Games/GamesReadDto.cs
+++ DHwD_web/Dtos/Games/GamesReadDto.cs
@@ -6,7 +6,8 @@ namespace DHwD_web.Dtos
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string description { get; set; }
+        public string Description { get; set; }
+        public string RecommendedStartingPlace { get; set; }
         public DateTime DateTimeStart { get; set; }
         public DateTime DateTimeEnd { get; set; }
     }
